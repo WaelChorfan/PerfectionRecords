e40c456bdd31806af0ea5ddbbee0f1cc2b1add62
Wael Chorfan <36275385+WaelChorfan@users.noreply.github.com>
2020-08-11 02:24:18 +0200

    Update dbconfig.php

--- SessionAdmin/dbconfig.php
+++ SessionAdmin/dbconfig.php
@@ -1,10 +1,15 @@
 <?php
 
-	$DB_HOST = 'localhost';
-	$DB_USER = 'root';
-	$DB_PASS = '';
-	$DB_NAME = 'base_musique';
+	$DB_HOST = 'remotemysql.com';
+	$DB_USER = 'uWrmz49yk9';
+	$DB_PASS = 'jqjUc8acER';
+	$DB_NAME = 'uWrmz49yk9';
 	
+
+// 	$DB_HOST = 'localhost';
+// 	$DB_USER = 'root';
+// 	$DB_PASS = '';
+// 	$DB_NAME = 'base_musique';
 	try{
 		$DB_con = new PDO("mysql:host={$DB_HOST};dbname={$DB_NAME}",$DB_USER,$DB_PASS);
 		$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
